8c361f98cfbca48ed73a5a1dd95a115cd8e5e913
Marcus Vinícius Bizerra Moreira <100208378+MarcusVBMoreira@users.noreply.github.com>
2023-02-07 11:08:06 -0300

    Add files via upload

---
+++ Aula_1/code.js
@@ -0,0 +1,25 @@
+let num1 = 4
+let num2 = 3
+
+function soma(n1,n2){
+    return n1 + 2
+}
+
+function sub(){
+    return n1 - 2
+}
+
+function mult(){
+    return n1 * 2
+}
+
+function div(){
+    return n1 / 2
+}
+
+
+
+total = num1 + num2
+
+console.log(soma(4,3))
+
